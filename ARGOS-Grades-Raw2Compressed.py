c9c241261e79af61ec99b063ed62513a254d9701
chmbrlnd <chmbrlnd@gmail.com>
2025-07-24 20:22:38 -0500

    Adding scripts for ARGOS processing.

---
+++ ARGOS-Grades-Raw2Compressed.py
@@ -0,0 +1,140 @@
+"""
+ARGOS - Adhoc Report Generation Output Solution
+
+This script reads CSV files and generates CSV files in a format suitable for Pandas.
+The original reports are generated through ARGOS (https://howdy.tamu.edu/) using the routine
+called 'ARGOS_Grades_Raw' on the VOAL virtual machine.
+Any CSV report in the proper format can act as an input to this script for further processing.
+
+Input Columns:
+    Campus, College, Dept, Subject, Number, Term, Section, CRN,
+    TIN, UIN, FirstName, LastName, Email, Primary,
+    Census, Enrollment, SCH, Method, Type, LectureMin, LabMin
+    StudentUIN, Credits, Grade
+"""
+
+
+import os
+import glob
+import pandas as pd
+import numpy as np
+
+# Define grade points
+grade_points = {'A': 4.0, 'B': 3.0, 'C': 2.0, 'D': 1.0, 'F': 0.0}
+
+# List desired CSV files in the directory
+college_id = 'EN'  # Engineering College ID
+base_path = 'ARGOS_Grades_Raw'
+output_path = 'ARGOS_Grades_Compressed'
+grade_files = glob.glob(os.path.join(base_path, f'ARGOS-Grades-{college_id}-*.csv'))
+
+# Group by relevant columns
+group_columns = [
+    'Campus', 'College', 'Dept', 'Subject', 'Number', 'Term',
+    'TIN', 'UIN', 'FirstName', 'LastName', 'Email',
+    'Type', 'LectureMin', 'LabMin'
+]
+
+output_columns = [
+    'Campus', 'College', 'Dept', 'Subject', 'Number', 'Term',
+    'TIN', 'UIN', 'FirstName', 'LastName', 'Email',
+    'Enrollment', 'Count', 'GPA', 'STD', 'InstructorSCH',
+    'Type', 'LectureMin', 'LabMin'
+]
+
+for file in grade_files:
+    # Extract filename
+    filename = os.path.basename(file)
+    # Extract the term part: removes prefix and suffix
+    term = filename.replace(f'ARGOS-Grades-{college_id}-', '').replace('.csv', '')
+    # Build output filename
+    output_filename = f'ARGOS-Grades-{college_id}-{term}_compressed.csv'
+    # Full output path
+    output_file = os.path.join(output_path, output_filename)
+    print('Processing File: ' + file)
+
+    # Read the CSV file into a DataFrame
+    grades_df = pd.read_csv(file)
+    grades_df = grades_df.drop(columns=['CRN', 'Census', 'Enrollment'])
+    print(f'grades_df size: {len(grades_df)}')
+    # Filter rows where 'Primary' is 'Y'
+    grades_df = grades_df[grades_df['Primary'] == 'Y']
+    print(f'grades_df size after Primary pruning: {len(grades_df)}')
+    # Filter rows where 'Method' is 'TR' or equivalent
+    grades_df = grades_df[(grades_df['Method'].isin(['TR', 'REM', 'DUAL', 'MIXED','WEB']))]
+    # grades_df = grades_df[(grades_df['Type'].isin(['LEC', 'LAB', 'LL']))]
+    print(f'grades_df size after Method and Type pruning: {len(grades_df)}')
+    grades_df = grades_df.drop(columns=['Primary', 'Method'])
+    # By default, rows with NaN in any of the grouping columns are omitted from the groupby() output entirely.
+    grades_df['LabMin'] = grades_df['LabMin'].fillna(0)
+    grades_df['LectureMin'] = grades_df['LectureMin'].fillna(0)
+
+    grades_df[['LectureMin', 'LabMin', 'Credits']] = grades_df[['LectureMin', 'LabMin', 'Credits']].apply(pd.to_numeric,
+                                                                                              errors='coerce')
+    InstructorCredits_vals = []
+    for _, row in grades_df.iterrows():
+        if row['Type'] in ['LEC']:
+            InstructorCredits = np.minimum(row['Credits'], 3)
+        elif row['Type'] in ['LAB']:
+            InstructorCredits = np.minimum(row['Credits']/3, 3)
+        elif row['Type'] in ['LL']:
+            InstructorCredits = np.minimum(np.minimum(row['Credits'], row['LectureMin']), 3)
+        else:
+            InstructorCredits = np.minimum(row['Credits'], 3)
+            type = row['Type']
+        InstructorCredits_vals.append(InstructorCredits)
+    grades_df['InstructorCredits'] = InstructorCredits_vals
+    print(f'InstructorCredits_vals size: {len(InstructorCredits_vals)}')
+
+    # Map grades to grade points, setting invalid grades to NaN
+    grades_df['GP'] = grades_df['Grade'].map(grade_points).where(grades_df['Grade'].isin(grade_points.keys()), np.nan)
+
+    # Calculate grade counts
+    grades_df[group_columns + ['Section']] = grades_df[group_columns + ['Section']].astype('string')
+
+    # Compute enrollment
+    grade_counts_df = grades_df.groupby(group_columns)['Grade'].value_counts().unstack(fill_value=0)
+    grade_counts_df['Enrollment'] = grade_counts_df.sum(axis=1)
+    grade_counts_df = grade_counts_df.reset_index()
+
+    # Compute student credit hours (SCH)
+    grades_df['SCH'] = pd.to_numeric(grades_df['SCH'], errors='coerce')
+    section_df = grades_df[group_columns + ['Section', 'SCH']].copy()
+    # Only the first occurrence of each unique combination of group_columns + ['Section']
+    section_grouped_df = section_df.groupby(group_columns + ['Section'], as_index=False).agg({
+        'SCH': 'first'
+    })
+    grade_sch_df = section_grouped_df.groupby(group_columns)['SCH'].sum().reset_index()
+
+    # Compute 'Count', 'GPA', 'STD', and 'InstructorSCH'
+    grades_df[['GP', 'InstructorCredits']] =  grades_df[['GP', 'InstructorCredits']].apply(pd.to_numeric, errors='coerce')
+    data_gpa_sch_df = grades_df.groupby(group_columns).agg(
+        Count=('GP', 'count'),
+        GPA=('GP', 'mean'),        # Mean GPA
+        STD=('GP', 'std'),        # Standard Deviation of GPA
+        InstructorCreditSum=('InstructorCredits', 'sum')
+    )
+    # For convenience, set STD to 0 when Count is 1
+    data_gpa_sch_df['STD'] = data_gpa_sch_df.apply(lambda row: 0 if row['Count'] <= 1 else row['STD'], axis=1)
+    data_gpa_sch_df = data_gpa_sch_df.reset_index()
+
+    # InstructorSCH should not exceed SCH
+    data_gpa_sch_df['InstructorSCH'] = np.minimum(data_gpa_sch_df['InstructorCreditSum'], grade_sch_df['SCH'])
+
+    # Flag rows where the minimum was from 'SCH'
+    flag_series = data_gpa_sch_df['InstructorSCH'] < data_gpa_sch_df['InstructorCreditSum']
+    flag_count = flag_series.sum()
+    print(f'Flag for InstructorSCH less than InstructorCredits: {flag_count}')
+    # flagged_courses = data_gpa_sch_df[flag_series]
+    # print(flagged_courses[['Subject', 'Number']])
+
+    # Combine GPA data with grade counts
+    results_df = pd.merge(grade_counts_df, data_gpa_sch_df, on=group_columns, how='outer')
+    results_df = pd.merge(results_df, grade_sch_df, on=group_columns, how='outer')
+    results_df = results_df.reset_index()
+
+    results_df = results_df[output_columns + ['SCH', 'InstructorCreditSum']]
+
+    # Display the final result
+    print(f'Saving File: {output_file}')
+    results_df.to_csv(output_file, index=False)
---
+++ DATA-Assessment2Evaluations.py
@@ -0,0 +1,180 @@
+"""
+This script gathers the necessary information to create the visual reports.
+It takes as input
+    1. ARGOS_Grades_Compressed CSV files
+    2. ASSESSMENT_Processed CSV files
+The former CSV files contain grade information, and the latter have the student feedback.
+
+The section information between ARGOS and ASSESSMENT (AEFIS/OIEE) often do not match.
+The section information is unreliable and must be dropped.
+"""
+
+from pathlib import Path
+import pandas as pd
+import numpy as np
+
+
+########################################################################################################################
+
+def load_csv(file_path):
+    if not file_path.exists():
+        print(f"File not found: {file_path}")
+        return None
+    return pd.read_csv(file_path)
+
+
+# Define a function to calculate weighted average and combined stddev for multiple questions
+def combine_stats(group, questions):
+    results = {}
+    for q in questions:
+        count = group[f'Q{q}_count']
+        avg = group[f'Q{q}_avg']
+        stddev = group[f'Q{q}_stddev']
+        # Total count
+        total_count = np.sum(count)
+        if total_count == 0:
+            # Handle case where total count is zero
+            results[f'Q{q}_count'] = 0
+            results[f'Q{q}_avg'] = np.nan  # No meaningful average
+            results[f'Q{q}_stddev'] = np.nan  # No meaningful standard deviation
+        else:
+            # Weighted average
+            weighted_avg = np.sum(count * avg) / total_count
+            # Combined standard deviation
+            combined_variance = (
+                np.sum(count * (stddev ** 2 + (avg - weighted_avg) ** 2)) / total_count
+            )
+            combined_stddev = np.sqrt(combined_variance)
+            # Store results
+            results[f'Q{q}_count'] = total_count
+            results[f'Q{q}_avg'] = weighted_avg
+            results[f'Q{q}_stddev'] = combined_stddev
+    return pd.Series(results)
+
+########################################################################################################################
+
+
+term_list = [
+    '202031', '202111', '202121',
+    '202131', '202211', '202221',
+    '202231', '202311', '202321',
+    '202331', '202411', '202421',
+    '202431', '202511'
+]
+# Merge Keys
+merge_keys = ['Dept', 'Term', 'Code', 'Number', 'LastName', 'FirstName', 'UIN']
+
+questions = [3, 4, 5, 7, 8, 9]
+assessment_course_columns = ['Dept', 'Year', 'Semester', 'Code', 'Number', 'Term', 'Level']
+assessment_instructor = ['LastName', 'FirstName', 'FullName', 'Instructor', 'UIN']
+assessment_questions = [f'Q{i}_count' for i in questions] + [f'Q{i}_avg' for i in questions] + [f'Q{i}_stddev' for i in questions]
+assessment_participation = ['Enrollment', 'Responses']
+assessment_numeric = assessment_participation + assessment_questions
+assessment_columns = assessment_course_columns + assessment_instructor + assessment_numeric
+grouped_keys = assessment_course_columns + assessment_instructor
+
+argos_columns = ['College', 'Dept', 'Code', 'Number', 'Term',
+                 'TIN', 'UIN', 'FirstName', 'LastName', 'Email',
+                 'Total', 'Count', 'GPA', 'STD', 'InstructorSCH']
+new_column_dict = {'Subject': 'Code', 'Enrollment': 'Total'}
+
+college_id = 'EN'  # Engineering College ID
+argos_grades_path = 'ARGOS_Grades_Compressed'
+assessment_path = 'ASSESSMENT_Processed'
+
+
+# assessment_frames = []
+for term in term_list:
+    print(f'Term: {term}')
+
+    ###########################################################################################################
+
+    # Load ARGOS Grades
+    argos_file = Path(argos_grades_path + f'/ARGOS-Grades-{college_id}-{term}_compressed.csv')
+    print(f'Processing File: {argos_file}')
+    argos_df = load_csv(argos_file)
+    if argos_df is None:
+        continue
+    print(f'ARGOS: {len(argos_df)}')
+
+    # Rename and drop columns as appropriate
+    argos_df.rename(columns=new_column_dict, inplace=True)
+    argos_df = argos_df[argos_columns]
+    # Convert numeric columns to numeric type
+    argos_numeric = ['Count', 'GPA', 'STD', 'InstructorSCH']
+    argos_df[argos_numeric] = argos_df[argos_numeric].apply(pd.to_numeric, errors='coerce')
+
+    ###########################################################################################################
+
+
+    # Load ASSESSMENT evaluations
+    assessment_file = Path(assessment_path +f'/ASSESSMENT-{college_id}-{term}.csv')
+    print(f'Processing File: {assessment_file}')
+    assessment_df = load_csv(assessment_file)
+    if assessment_df is None:
+        continue
+    else:
+        assessment_df.drop('Email', axis=1, inplace=True, errors='ignore')
+    print(f'ASSESSMENT: {len(assessment_df)}')
+
+    # EXCLUDE: WEB sections start with a seven.
+    assessment_df = assessment_df[~assessment_df['Section'].astype(str).str.startswith('7')]
+    # Convert numeric columns to numeric type
+    assessment_df = assessment_df[assessment_columns]
+    assessment_df[assessment_numeric] = assessment_df[assessment_numeric].apply(pd.to_numeric, errors='coerce')
+
+    ###########################################################################################################
+    # Section numbers do not always match between ASSESSMENT (AEFIS or OIEE), and ARGOS.
+    ###########################################################################################################
+
+    # Check if all entries in merged_numeric are present in merged_df.columns
+    required_columns = set(assessment_numeric)
+    existing_columns = set(assessment_df.columns)
+    missing_columns = required_columns - existing_columns
+    if missing_columns:
+        print(f"Missing columns in assessment_df: {missing_columns}")
+        continue
+    elif assessment_df.empty:
+        print("DataFrame assessment_df is empty.")
+        continue
+    else:
+        grouped_stats_df = assessment_df.groupby(grouped_keys)[assessment_numeric].apply(combine_stats, questions=questions).reset_index(drop=False)
+        print(f'grouped_stats_df: {len(grouped_stats_df)}')
+
+        grouped_sum_df = assessment_df[assessment_course_columns + assessment_instructor + assessment_participation]
+        # Group the DataFrame and apply function
+        grouped_sum_df = grouped_sum_df.groupby(grouped_keys).sum().reset_index()
+        # grouped_df[assessment_questions] = grouped_df[assessment_questions].divide(grouped_df['Responses'], axis='index')
+        print(f'grouped_sum_df: {len(grouped_sum_df)}')
+
+        grouped_df = pd.merge(grouped_stats_df, grouped_sum_df, on=grouped_keys, how='left')
+        print(f'DataFrame after merge: grouped_df of length {str(len(grouped_df))}')
+
+    ###########################################################################################################
+
+    missing_keys_argos = set(merge_keys) - set(argos_df.columns)
+    missing_keys_grouped = set(merge_keys) - set(grouped_df.columns)
+    if missing_keys_argos:
+        print(f"Missing keys in ARGOS DataFrame: {missing_keys_argos}")
+        continue
+    elif missing_keys_grouped:
+        print(f"Missing keys in grouped DataFrame: {missing_keys_grouped}")
+        continue
+    else:
+        evaluation_df = pd.merge(grouped_df, argos_df, on=merge_keys, how='left')
+        print(f'DataFrame after merge: evaluation_df of length {str(len(grouped_df))}')
+
+    ###########################################################################################################
+    # Sometimes there are discrepancies between the primary instructor in ASSESSMENT and ARGOS.
+    # In such cases, the row is dropped.
+    # Courses taught as Satisfactory/Unsatisfactory courses without a GPA are dropped.
+    ###########################################################################################################
+    print(f'DataFrame after ARGOS merge: evaluation_df of length {str(len(evaluation_df))}')
+    # Drop rows with NaN values in 'GPA' column
+    evaluation_df = evaluation_df.dropna(subset=['GPA'])
+    # Drop rows with NaN values in 'UIN' column
+    evaluation_df = evaluation_df.dropna(subset=['UIN'])
+    print('DataFrame after empty UIN drop: evaluation_df of length ' + str(len(evaluation_df)))
+    # evaluation_df.to_csv('DATA_Evaluations/' + dept + '_evaluation.csv', index=False)
+    evaluation_df.to_csv(f'DATA_Evaluations/Data-{college_id}-{term}.csv', index=False)
+    print(f'Saving File: DATA_Evaluations/Data-{college_id}-{term}.csv')
